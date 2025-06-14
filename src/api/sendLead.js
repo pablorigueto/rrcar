d11e68d9e9b63c9e5199dc0f03da307fb07ac54e
p1 <pabloedurigueto@outlook.com>
2025-06-14 18:31:28 -0300

    Keep going.

--- src/api/sendLead.js
+++ src/api/sendLead.js
@@ -4,7 +4,16 @@ import axios from 'axios';
 
 export const sendLead = async (data) => {
   try {
-    const response = await axios.post('https://seu-endpoint-aqui/api/lead', data);
+
+    const protocol = window.location.protocol;
+    const host = window.location.host;
+    const pathname = '/data/api.php'; // New pathname to merge.
+    // Create the new URL without query parameters
+    const newURL = `${protocol}//${host}${pathname}`;
+
+    //const response = await axios.post(newURL, data);
+    const response = await axios.post('http://127.0.0.1/rrcar/api.php', data);
+
     return response.data;
   } catch (error) {
     console.error('Erro ao enviar lead:', error);
